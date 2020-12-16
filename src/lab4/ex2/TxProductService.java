a34e0109a1d29675e0ce6b47b861678bcc5d3a61
Admin <Admin@K-15>
2020-12-16 11:32:35 +0800

    transaction manager

---
+++ src/lab4/ex2/TxProductService.java
@@ -0,0 +1,21 @@
+package lab4.ex2;
+
+public class TxProductService {
+
+	private ProductDao productDao;
+
+	public void setProductDao(ProductDao productDao) {
+		this.productDao = productDao;
+	}
+	
+	public void testTx(){
+		Product p = productDao.getProduct(1);
+		p.setName("Frankie-tx");
+		productDao.update(p);
+		//raiseException();
+	}
+	public void raiseException(){
+		throw new RuntimeException();
+	}
+
+}
